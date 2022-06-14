ee5650b5855d96c499d75a3c22b76e34edc63a7e
P_P_P_P_P <ppzzhh1186861238@hotmail.com>
2022-06-14 15:09:38 +0800

    debug

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ with requests.get(download_url, stream=True) as r:
 z = zipfile.ZipFile("./recorder.zip")
 z.extractall()
 shutil.move("./any/Release","./executeable")
-if sys.platform == "linux" or sys.platform == "darwin":  subprocess.run("sudo chmod +x ./executeable/BililiveRecorder.Cli", shell=False)
+if sys.platform == "linux" or sys.platform == "darwin":  subprocess.run("sudo chmod +x ./executeable/BililiveRecorder.Cli", shell=True)
 command = ["./executeable/BililiveRecorder.Cli","run","--bind","http://*:2345","output"]
 Record_Process = subprocess.Popen(' '.join(command), shell=True ,stdout=subprocess.PIPE)
 
